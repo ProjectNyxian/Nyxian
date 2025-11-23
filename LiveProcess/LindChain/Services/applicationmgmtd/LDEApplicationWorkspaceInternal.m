ProjectNyxian/Nyxian

commit 30fabfe413cf110db7ed97299a7a4aeee240ad16

Fixing a vulnerability where a process can replace its own structure while something else replaced it

Nyxian/LindChain/ProcEnvironment/Server/ServerSession.m
@@ -181,6 +181,8 @@
                   withIdentifier:(unsigned int)uid
                        withReply:(void (^)(unsigned int result))reply
 {
+    reflock_lock(&(surface->reflock));
+    
     ksurface_proc_t proc = {};
     ksurface_error_t error = proc_for_pid(_processIdentifier, &proc);
     
@@ -238,6 +240,9 @@
     }
     
     reply((error == kSurfaceErrorSuccess) ? 0 : -1);
+    
+    reflock_unlock(&(surface->reflock));
+    
     return;
 }
 

commit ca33e001ab9ab80015b29e76bf7b36b7775dfe40

Fixed a deadlock caused by https://github.com/ProjectNyxian/Nyxian/commit/30fabfe413cf110db7ed97299a7a4aeee240ad16

LiveProcess/LindChain/Services/applicationmgmtd/LDEApplicationWorkspaceInternal.m
@@ -255,8 +255,17 @@ bool checkCodeSignature(const char* path);
     NSURL *url = [NSURL fileURLWithPath:object.path];
     NSString *fastPath = [[[[LDEApplicationWorkspaceInternal shared] binaryURL] path] stringByAppendingPathComponent:[url lastPathComponent]];
     [object writeOut:[[[[LDEApplicationWorkspaceInternal shared] binaryURL] path] stringByAppendingPathComponent:[url lastPathComponent]]];
-    environment_proxy_sign_macho(fastPath);
-    reply(fastPath, checkCodeSignature([fastPath UTF8String]));
+    bool isSigned = checkCodeSignature([fastPath UTF8String]);
+    if(!isSigned)
+    {
+        environment_proxy_sign_macho(fastPath);
+        isSigned = checkCodeSignature([fastPath UTF8String]);
+        if(!isSigned)
+        {
+            [[NSFileManager defaultManager] removeItemAtPath:fastPath error:nil];
+        }
+    }
+    reply(fastPath, isSigned);
 }
 
 @end

LiveProcess/LindChain/Services/trustd/LDETrust.h
@@ -1,0 +1,18 @@
+/*
+ Copyright (C) 2025 cr4zyengineer
+
+ This file is part of Nyxian.
+
+ Nyxian is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Nyxian is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Nyxian. If not, see <https://www.gnu.org/licenses/>.
+*/

LiveProcess/LindChain/Services/trustd/LDETrust.m
@@ -1,0 +1,18 @@
+/*
+ Copyright (C) 2025 cr4zyengineer
+
+ This file is part of Nyxian.
+
+ Nyxian is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Nyxian is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Nyxian. If not, see <https://www.gnu.org/licenses/>.
+*/

LiveProcess/LindChain/Services/trustd/LDETrustInternal.h
@@ -1,0 +1,7 @@
+//
+//  LDETrustInternal.h
+//  Nyxian
+//
+//  Created by FridaDEV on 23.11.25.
+//
+

LiveProcess/LindChain/Services/trustd/LDETrustInternal.m
@@ -1,0 +1,7 @@
+//
+//  LDETrustInternal.m
+//  Nyxian
+//
+//  Created by FridaDEV on 23.11.25.
+//
+

LiveProcess/LindChain/Services/trustd/LDETrustProtocol.h
@@ -1,0 +1,18 @@
+/*
+ Copyright (C) 2025 cr4zyengineer
+
+ This file is part of Nyxian.
+
+ Nyxian is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Nyxian is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Nyxian. If not, see <https://www.gnu.org/licenses/>.
+*/

Nyxian/LindChain/ProcEnvironment/Server/ServerSession.m
@@ -189,6 +189,7 @@
     if(error != kSurfaceErrorSuccess)
     {
         reply(-1);
+        reflock_unlock(&(surface->reflock));
         return;
     }
     
@@ -218,6 +219,7 @@
             break;
         default:
             reply(-1);
+            reflock_unlock(&(surface->reflock));
             return;
     }
     
@@ -232,15 +234,18 @@
     if(processWasModified && processAllowedToElevate)
     {
         error = proc_replace(proc);
+        reply((error == kSurfaceErrorSuccess) ? 0 : -1);
+        reflock_unlock(&(surface->reflock));
+        return;
     }
     else if(processWasModified)
     {
         reply(-1);
+        reflock_unlock(&(surface->reflock));
         return;
     }
     
-    reply((error == kSurfaceErrorSuccess) ? 0 : -1);
-    
+    reply(0);
     reflock_unlock(&(surface->reflock));
     
     return;