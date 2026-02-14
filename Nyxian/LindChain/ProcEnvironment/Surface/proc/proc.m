cd4a36439ee53319c23caca160e06273c9bac226
Frida <157828245+cr4zyengineer@users.noreply.github.com>
2026-02-14 15:08:25 +0100

    the structure is nulled already

--- Nyxian/LindChain/ProcEnvironment/Surface/proc/proc.m
+++ Nyxian/LindChain/ProcEnvironment/Surface/proc/proc.m
@@ -46,7 +46,6 @@ DEFINE_KVOBJECT_INIT_HANDLER(proc)
         proc->kproc.kcproc.bsd.kp_eproc.e_flag = 2;
         proc->kproc.kcproc.bsd.kp_proc.p_stat = SRUN;
         proc->kproc.kcproc.bsd.kp_proc.p_flag = P_LP64 | P_EXEC;
-        proc->kproc.kcproc.nyx.sid = 0;
     }
     
     pthread_mutex_init(&(proc->kproc.children.mutex), NULL);
