768ef645c77c1cd261477bc59f3743fb4d6eb2fd
Frida <157828245+cr4zyengineer@users.noreply.github.com>
2026-03-10 12:57:10 +0100

    security: improved handling in ktfp(1)

--- Nyxian/LindChain/ProcEnvironment/Utils/ktfp.m
+++ Nyxian/LindChain/ProcEnvironment/Utils/ktfp.m
@@ -32,24 +32,24 @@ task_t ktfp(obtain_token_t token)
     
     if(token == KTFP_GUEST)
     {
-        /* preparing for handoff */
+        /*
+         * constructing the exception port and
+         * setting it to our task for the host,
+         * to receive the IKOT_TASK of the guest
+         * through it, therefore receiving full
+         * power over the guest.
+         */
         mach_port_options_t opt = {
             .flags =  MPO_PORT | MPO_INSERT_SEND_RIGHT
         };
         
-        /* constructing port */
         kr = mach_port_construct(mach_task_self(), &opt, 0, &exceptionPort);
-        
-        /* sanity check */
         if(kr != KERN_SUCCESS)
         {
             return MACH_PORT_NULL;
         }
         
-        /* setting it as exception port */
         kr = task_set_exception_ports(mach_task_self(), EXC_MASK_BREAKPOINT, exceptionPort, EXCEPTION_DEFAULT, ARM_THREAD_STATE64);
-        
-        /* sanity check */
         if(kr != KERN_SUCCESS)
         {
             goto out_dealloc;
@@ -59,8 +59,11 @@ task_t ktfp(obtain_token_t token)
         environment_syscall(SYS_handoffep, exceptionPort);
         
         /*
-         * this trap will be sent by the iOS kernel it self
-         * to the exception port of nyxian.
+         * this causes EXC_BREAKPOINT, which causes
+         * a mach exception which will be sent to the
+         * exception port which the host now holds the
+         * receive right of meaning the host will
+         * handle this pseudo exception.
          */
         __builtin_trap();
         
@@ -83,111 +86,106 @@ task_t ktfp(obtain_token_t token)
         exceptionPort = token;
     }
     
+    /* host handling of guest exception port */
+    
     /* will carry request buffer */
-    __Request__exception_raise_t *request = NULL;
-    size_t request_size = round_page(sizeof(*request));
+    __Request__exception_raise_t request;
+    size_t request_size = round_page(sizeof(request));
     mach_msg_return_t mr;
     
-    /* allocating request buffer via mach, page alligned */
-    kr = vm_allocate(mach_task_self(), (vm_address_t *) &request, request_size, VM_FLAGS_ANYWHERE);
-    if(kr != KERN_SUCCESS)
-    {
-        klog_log(@"ktfp", @"allocating request buffer failed: %s", mach_error_string(kr));
-        goto out_destroy_recv;
-    }
+    /* receiving pseudo exception caused by guest */
+    request.Head.msgh_local_port = exceptionPort;
+    request.Head.msgh_size = (mach_msg_size_t)request_size;
+    mr = mach_msg(&(request.Head), MACH_RCV_MSG | MACH_RCV_LARGE | MACH_RCV_TIMEOUT, 0, request.Head.msgh_size, exceptionPort, 1000, MACH_PORT_NULL);
     
-    // Now requesting the message and waiting on a reply from the kernel.. happens on exception
-    request->Head.msgh_local_port = exceptionPort;
-    request->Head.msgh_size = (mach_msg_size_t)request_size;
-    mr = mach_msg(&(request->Head), MACH_RCV_MSG | MACH_RCV_LARGE | MACH_RCV_TIMEOUT, 0, request->Head.msgh_size, exceptionPort, 1000, MACH_PORT_NULL);
+    /* predefined variable */
+    task_t exportedTask = MACH_PORT_NULL;
     
-    if(mr == MACH_RCV_TIMED_OUT)
-    {
-        klog_log(@"ktfp", @"attempt on aquiring task port timed out");
-        goto out_destroy_recv;
-    }
-    else if(mr != MACH_MSG_SUCCESS)
+    /* handling timeout and other errors */
+    if(mr != MACH_MSG_SUCCESS)
     {
         klog_log(@"ktfp", @"failed receiving task port");
-        goto out_destroy_recv;
+        goto out_destroy_request;
     }
     
     /* task port validation */
     ipc_info_object_type_t type;
     mach_vm_address_t address;
-    kr = mach_port_kobject(mach_task_self(), request->task.name, &type, &address);
+    kr = mach_port_kobject(mach_task_self(), request.task.name, &type, &address);
     
     if(kr != KERN_SUCCESS)
     {
         klog_log(@"ktfp", @"failed getting the kobject type");
-        goto out_destroy_recv;
+        goto out_destroy_request;
     }
     
     /* checking for ipc object type */
     if(type != IPC_OTYPE_TASK_CONTROL)  /* also known as IKOT_TASK.. aka kernel task port */
     {
-        klog_log(@"ktfp", @"port %d holding ipc object with type %d is not a IKOT_TASK", request->task.name, type);
-        goto out_destroy_recv;
+        klog_log(@"ktfp", @"port %d holding ipc object with type %d is not a IKOT_TASK", request.task.name, type);
+        goto out_destroy_request;
     }
     
-    /* now manipulate thread state */
+    /*
+     * now manipulate thread state of the thread
+     * in the guest that has the exception, because
+     * its a pseudo exception and if we wont skip
+     * over the pseudo exception it will reexecute
+     * it or crash.
+     */
     arm_thread_state64_t state;
     mach_msg_type_number_t count = ARM_THREAD_STATE64_COUNT;
-    kr = thread_get_state(request->thread.name, ARM_THREAD_STATE64, (thread_state_t)&state, &count);
+    kr = thread_get_state(request.thread.name, ARM_THREAD_STATE64, (thread_state_t)&state, &count);
     
     if(kr != KERN_SUCCESS)
     {
         klog_log(@"ktfp", @"failed to get thread state");
-        goto out_destroy_recv;
+        goto out_destroy_request;
     }
     
+    /* skipping over pseudo exception */
     state.__pc += 4;
     
-    kr = thread_set_state(request->thread.name, ARM_THREAD_STATE64, (thread_state_t)&state, count);
+    
+    kr = thread_set_state(request.thread.name, ARM_THREAD_STATE64, (thread_state_t)&state, count);
     
     if(kr != KERN_SUCCESS)
     {
         klog_log(@"ktfp", @"failed to restore thread state");
-        goto out_destroy_recv;
+        goto out_destroy_request;
     }
     
-    task_t exportedTask = request->task.name;
-    
-    kr = mach_port_mod_refs(mach_task_self(), exportedTask, MACH_PORT_RIGHT_SEND, 1);
+    kr = mach_port_mod_refs(mach_task_self(), request.task.name, MACH_PORT_RIGHT_SEND, 1);
     
     if(kr != KERN_SUCCESS)
     {
         klog_log(@"ktfp", @"failed to increment tsdk port send right");
-        goto out_destroy_recv;
+        goto out_destroy_request;
     }
+    
+    exportedTask = request.task.name;
     
     /* after replying the kernel will happily continue executing the task */
     __Reply__exception_raise_t reply;
     memset(&reply, 0, sizeof(reply));
-    reply.Head.msgh_bits = MACH_MSGH_BITS(MACH_MSGH_BITS_REMOTE(request->Head.msgh_bits), 0);
-    reply.Head.msgh_id = request->Head.msgh_id + 100;
+    reply.Head.msgh_bits = MACH_MSGH_BITS(MACH_MSGH_BITS_REMOTE(request.Head.msgh_bits), 0);
+    reply.Head.msgh_id = request.Head.msgh_id + 100;
     reply.Head.msgh_local_port = MACH_PORT_NULL;
-    reply.Head.msgh_remote_port = request->Head.msgh_remote_port;
+    reply.Head.msgh_remote_port = request.Head.msgh_remote_port;
     reply.Head.msgh_size = sizeof(reply);
     reply.NDR = NDR_record;
     reply.RetCode = kr;
     mr = mach_msg(&reply.Head, MACH_SEND_MSG, reply.Head.msgh_size, 0, MACH_PORT_NULL, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
     
-    /* destroying mach message */
-    mach_msg_destroy(&(request->Head));
-    
     if(mr != KERN_SUCCESS)
     {
         klog_log(@"ktfp", @"failed to reply back to guest");
-        goto out_destroy_recv;
     }
     
+out_destroy_request:
+    mach_msg_destroy(&(request.Head));
     mach_port_mod_refs(mach_task_self(), exceptionPort, MACH_PORT_RIGHT_RECEIVE, -1);
     return exportedTask;
-
-out_destroy_recv:
-    mach_port_mod_refs(mach_task_self(), exceptionPort, MACH_PORT_RIGHT_RECEIVE, -1);
-    return MACH_PORT_NULL;
 }
 
 #endif /* !JAILBREAK_ENV */
