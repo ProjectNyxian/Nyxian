610ec402d7a593c031d301b7a651a83000b21142
cr4zyengineer <fridakitten@pm.me>
2026-03-11 14:20:01 +0100

    refractor: added AGPL license to the existing LICENSE

--- Nyxian/LindChain/LiveContainer/Tweaks/Tweaks.h
+++ Nyxian/LindChain/LiveContainer/Tweaks/Tweaks.h
@@ -17,7 +17,7 @@
  GNU Affero General Public License for more details.
 
  You should have received a copy of the GNU Affero General Public License
- along with Nyxian. If not, see <https://www.gnu.org/licenses/>.
+ along with LiveContainer. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #ifndef TWEAKS_TWEAKS_H
