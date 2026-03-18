8adabd34afe0cc5399be670bd7b64863f5279822
Frida <157828245+cr4zyengineer@users.noreply.github.com>
2026-03-18 01:01:49 +0100

    refractor: improved documentation in posix_spawn fix

--- Nyxian/LindChain/ProcEnvironment/posix_spawn.m
+++ Nyxian/LindChain/ProcEnvironment/posix_spawn.m
@@ -199,111 +199,165 @@ int environment_posix_spawn(pid_t *process_identifier,
                             const posix_spawnattr_t *spawn_attr,
                             char *const argv[],
                             char *const envp[])
-{    
-    /* resolving path of executable */
+{
+    environment_must_be_role(EnvironmentRoleGuest);
+    
+    /*
+     * resolving realpath of the executable, to prevent
+     * weird file bugs to happen, this is standard
+     * apple validity. I got no idea tbh.
+     */
     char resolved[PATH_MAX];
     realpath(path, resolved);
     
-    if(environment_is_role(EnvironmentRoleGuest))
+    /*
+     * checking code signature of resolved binary,
+     * otherwise it wont be able to run.
+     */
+    if(!checkCodeSignature(resolved))
     {
-        /* checking code signature of resolved binary */
-        if(!checkCodeSignature(resolved))
+        /* attempt signing */
+        int ret = (int)environment_syscall(SYS_signexec, resolved);
+        
+        if(ret != 0)
         {
-            /* attempt signing */
-            int ret = (int)environment_syscall(SYS_signexec, resolved);
-            
-            if(ret != 0)
-            {
-                return ret;
-            }
-            
-            /* checking if kernel virt signed executable */
-            if(!checkCodeSignature(resolved))
-            {
-                return -1;
-            }
-            
-            /* for some reason we get a iOS panic otherwise */
-            if(![FDObject forceVnodeReassignment:[NSString stringWithCString:resolved encoding:NSUTF8StringEncoding]])
-            {
-                return -1;
-            }
-        }
-        
-        /* create fd map object or take it */
-        FDMapObject *mapObject = [FDMapObject currentMap];
-        
-        /* getting cwd */
-        char cwd[PATH_MAX] = {};
-        
-        if(fa == NULL)
-        {
-            goto skip_fileactions;
-        }
-        
-        /* interpretting file actions :3c */
-        _posix_spawn_file_actions_t *faPtr = (_posix_spawn_file_actions_t*)fa;
-        
-        for(uint64_t i = 0; i < (*faPtr)->psfa_act_count; i++)
-        {
-            switch((*faPtr)->psfa_act_acts[i].psfaa_type)
-            {
-                case PSFA_OPEN:
-                    [mapObject openWithFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes withPath:(*faPtr)->psfa_act_acts[i].psfaa_openargs.psfao_path withFlags:(*faPtr)->psfa_act_acts[i].psfaa_openargs.psfao_oflag withMode:(*faPtr)->psfa_act_acts[i].psfaa_openargs.psfao_mode];
-                    break;
-                case PSFA_CLOSE:
-                    [mapObject closeWithFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes];
-                    break;
-                case PSFA_DUP2:
-                    [mapObject dup2WithOldFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes withNewFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_dup2args.psfad_newfiledes];
-                    break;
-                case PSFA_INHERIT:
-                    [mapObject appendFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes];
-                    break;
-                case PSFA_FILEPORT_DUP2:
-                    /* S0n */
-                    break;
-                case PSFA_CHDIR:
-                    /* not available on iOS but shrug, add it anyways */
-                    strlcpy(cwd, (*faPtr)->psfa_act_acts[i].psfaa_chdirargs.psfac_path, PATH_MAX);
-                    break;
-                case PSFA_FCHDIR:
-                    /* S0n */
-                    break;
-            }
-        }
-        
-    skip_fileactions:
-        if(cwd[0] == '\0')
-        {
-            getcwd(cwd, PATH_MAX);
-        }
-        
-        NSString *nsCwd = [NSString stringWithCString:cwd encoding:NSUTF8StringEncoding];
-        
-        /* trying to spawn process */
-        int64_t pid = environment_proxy_spawn_process_at_path([NSString stringWithCString:resolved encoding:NSUTF8StringEncoding], createNSArrayFromArgv(argv), EnvironmentDictionaryFromEnvp(envp), mapObject, nsCwd);
-        
-        /* return (if its negative then its not a valid pid) */
-        if(pid == -1)
-        {
-            return -1;
-        }
-        
-        /* sanity check */
-        if(process_identifier != NULL)
-        {
-            *process_identifier = (pid_t)pid;
+            /*
+             * errno comes from the syscall in this case
+             * errno = EPERM;
+             */
+            return ret;
         }
         
         /*
-         * shitty soloution for now (to for now fix waitpid)
-         * most delay was fixed by making sure it runs
-         * synchronised but this doesnt run synchronised..
-         * meaning SYS_gettask.
+         * checking if kernel virt actually signed
+         * executable.
          */
-        usleep(50000);
+        if(!checkCodeSignature(resolved))
+        {
+            errno = EBADEXEC;
+            return -1;
+        }
+        
+        /* for some reason we get a iOS kernel panic otherwise */
+        if(![FDObject forceVnodeReassignment:[NSString stringWithCString:resolved encoding:NSUTF8StringEncoding]])
+        {
+            /*
+             * TODO: first copy, sign the copy and then copy the copy to the original path and delete the original copy
+             * sounds complex, but thats safety, to prevemt a iOS panic on all fronts. otherwise just the vnode
+             * reassignment could fail and the actual file stays in that stale state then triggering said panic.
+             */
+            errno = EIO;
+            return -1;
+        }
     }
+    
+    /*
+     * create new map object of all current file descriptors,
+     * because they are by default inherited by the new child
+     * process which is expected behaviour. the file actions
+     * can alter that stabily.
+     */
+    FDMapObject *mapObject = [FDMapObject currentMap];
+    
+    /*
+     * nullified cwd buffer, because of the case where a posix
+     * file actions sets the buffer via PSFA_CHDIR, if not we
+     * retrieve the cwd and use it instead
+     * as the cwd of the child process, which is expected posix
+     * behaviour.
+     */
+    char cwd[PATH_MAX] = {};
+    
+    if(fa == NULL)
+    {
+        /* preventing a NULL ptr deref. */
+        goto skip_fileactions;
+    }
+    
+    /* interpretting file actions :3c */
+    _posix_spawn_file_actions_t *faPtr = (_posix_spawn_file_actions_t*)fa;
+    
+    for(uint64_t i = 0; i < (*faPtr)->psfa_act_count; i++)
+    {
+        switch((*faPtr)->psfa_act_acts[i].psfaa_type)
+        {
+            case PSFA_OPEN:
+                [mapObject openWithFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes withPath:(*faPtr)->psfa_act_acts[i].psfaa_openargs.psfao_path withFlags:(*faPtr)->psfa_act_acts[i].psfaa_openargs.psfao_oflag withMode:(*faPtr)->psfa_act_acts[i].psfaa_openargs.psfao_mode];
+                break;
+            case PSFA_CLOSE:
+                [mapObject closeWithFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes];
+                break;
+            case PSFA_DUP2:
+                [mapObject dup2WithOldFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes withNewFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_dup2args.psfad_newfiledes];
+                break;
+            case PSFA_INHERIT:
+                [mapObject appendFileDescriptor:(*faPtr)->psfa_act_acts[i].psfaa_filedes];
+                break;
+            case PSFA_FILEPORT_DUP2:
+                /* S0n */
+                break;
+            case PSFA_CHDIR:
+                /* not available on iOS but shrug, add it anyways */
+                strlcpy(cwd, (*faPtr)->psfa_act_acts[i].psfaa_chdirargs.psfac_path, PATH_MAX);
+                break;
+            case PSFA_FCHDIR:
+                /* S0n */
+                break;
+        }
+    }
+    
+skip_fileactions:
+    
+    if(cwd[0] == '\0')
+    {
+        getcwd(cwd, PATH_MAX);
+    }
+    
+    /* the old ServerSession api requires paths as NSString */
+    NSString *nsCwd = [NSString stringWithCString:cwd encoding:NSUTF8StringEncoding];
+    
+    if(nsCwd == nil)
+    {
+        errno = EAGAIN;
+        return -1;
+    }
+    
+    /*
+     * trying to spawn process via old ass ServerSession API, which
+     * then triggers the subsystem LDEProcess on the host side.
+     */
+    int64_t pid = environment_proxy_spawn_process_at_path([NSString stringWithCString:resolved encoding:NSUTF8StringEncoding], createNSArrayFromArgv(argv), EnvironmentDictionaryFromEnvp(envp), mapObject, nsCwd);
+    
+    /* it shouldnt be negative */
+    if(pid == -1)
+    {
+        /* lacking entitlements? */
+        errno = EBADEXEC;
+        return -1;
+    }
+    
+    /* overwriting passed process identifier pointer if applicable */
+    if(process_identifier != NULL)
+    {
+        *process_identifier = (pid_t)pid;
+    }
+    
+    /*
+     * shitty soloution for now (to for now fix waitpid)
+     * most delay was fixed by making sure it runs
+     * synchronised, but the guest process sending its
+     * task port to the host side happens after
+     * ServerSession API returns. So to make sure
+     * a process waiting using waitpid on the guest
+     * to stop after SIGSTOP for example to debug it
+     * must be guaranteed to be able to get the other guest
+     * processes task port. this will likely change in the
+     * future by using the event api to wait on that task
+     * port of the guest in a other API, the problems with
+     * that tho is the nature of not knowing when that might
+     * happen, so this is the simplest solution for now.
+     */
+    usleep(50000);
     
     return 0;
 }
@@ -325,8 +379,6 @@ void environment_posix_spawn_init(void)
 {
     if(environment_is_role(EnvironmentRoleGuest))
     {
-        // MARK: GUEST Init
-        
         // MARK: Fixing spawning of child processes
         litehook_rebind_symbol(LITEHOOK_REBIND_GLOBAL, posix_spawn, environment_posix_spawn, nil);
         litehook_rebind_symbol(LITEHOOK_REBIND_GLOBAL, posix_spawnp, environment_posix_spawnp, nil);
