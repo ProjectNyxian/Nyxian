610d19abb9aba190df5eb0840871832ac4157c62
Frida <157828245+cr4zyengineer@users.noreply.github.com>
2026-03-23 14:25:16 +0100

    chore: improved documentation of syscall client

--- Nyxian/LindChain/ProcEnvironment/Syscall/mach_syscall_client.m
+++ Nyxian/LindChain/ProcEnvironment/Syscall/mach_syscall_client.m
@@ -51,28 +51,10 @@ static void reply_port_destructor(void *port_ptr)
     }
 }
 
-syscall_client_t *syscall_client_create(mach_port_t port)
-{
-    syscall_client_t *client = malloc(sizeof(syscall_client_t));
-    
-    if(client == NULL)
-    {
-        return NULL;
-    }
-    
-    client->server_port = port;
-    
-    if(pthread_key_create(&client->reply_port_key, reply_port_destructor) != 0)
-    {
-        free(client);
-        return NULL;
-    }
-    
-    return client;
-}
-
 static mach_port_t get_thread_reply_port(syscall_client_t *client)
 {
+    assert(client != NULL);
+    
     mach_port_t port = (mach_port_t)(uintptr_t)pthread_getspecific(client->reply_port_key);
     
     if(port == MACH_PORT_NULL)
@@ -88,32 +70,59 @@ static mach_port_t get_thread_reply_port(syscall_client_t *client)
             return MACH_PORT_NULL;
         }
         
+        /*
+         * set port as associated data of the thread
+         * so we can clean it up once the thread dies.
+         */
         pthread_setspecific(client->reply_port_key, (void*)(uintptr_t)port);
     }
     
     return port;
 }
 
+syscall_client_t *syscall_client_create(mach_port_t port)
+{
+    assert(port != MACH_PORT_NULL);
+    
+    syscall_client_t *client = malloc(sizeof(syscall_client_t));
+    
+    if(client == NULL)
+    {
+        return NULL;
+    }
+    
+    client->server_port = port;
+    
+    /*
+     * to make sure every thread gets the correct reply to its
+     * syscall we create a pthread key so every thread gets
+     * one syscall reply port, this port then gets cleaned up
+     * when the thread dies. good and scalable!
+     */
+    if(pthread_key_create(&client->reply_port_key, reply_port_destructor) != 0)
+    {
+        free(client);
+        return NULL;
+    }
+    
+    return client;
+}
+
 void syscall_client_destroy(syscall_client_t *client)
 {
     assert(client != NULL);
     
-    /* checking if the port is null */
     if(client->server_port != MACH_PORT_NULL)
     {
-        /* deallocate port */
         mach_port_deallocate(mach_task_self(), client->server_port);
     }
     
+    /* shall destroy all ports in existence */
     pthread_key_delete(client->reply_port_key);
     
     free(client);
 }
 
-/*
- * this symbol invokes the syscalls, it sends the request to the syscall server which then calls
- * the actual syscall
- */
 int64_t syscall_invoke(syscall_client_t *client,
                        uint32_t syscall_num,
                        int64_t *args,
@@ -123,8 +132,13 @@ int64_t syscall_invoke(syscall_client_t *client,
                        mach_port_t **out_ports,
                        uint32_t out_ports_cnt)
 {
-    assert(client != NULL);
+    assert(client != NULL && args != NULL);
     
+    /*
+     * getting thread specific reply port to
+     * use for environment syscalls, so there
+     * wont be any ghost replies.
+     */
     mach_port_t reply_port = get_thread_reply_port(client);
     if(reply_port == MACH_PORT_NULL)
     {
@@ -132,36 +146,46 @@ int64_t syscall_invoke(syscall_client_t *client,
         return -1;
     }
     
-    /* building syscall request :3c */
+    /*
+     * the request stack memory buffer for
+     * the host.
+     */
     syscall_msg_buffer_t buffer;
-    
-    /* nullfying buffer */
     bzero(&buffer, sizeof(buffer));
     
-    /* stuffing the request ;3 */
+    /* setting up request >~< */
     buffer.req.header.msgh_bits = MACH_MSGH_BITS(MACH_MSG_TYPE_COPY_SEND, MACH_MSG_TYPE_MAKE_SEND_ONCE);
     buffer.req.header.msgh_remote_port = client->server_port;
     buffer.req.header.msgh_local_port = reply_port;
     buffer.req.header.msgh_size = sizeof(syscall_request_t);
     buffer.req.header.msgh_id = syscall_num;
-    
-    /* telling cutie patootie ksurface what syscall we wanna call ^^ */
     buffer.req.syscall_num = syscall_num;
+    bcopy(args, buffer.req.args, sizeof(buffer.req.args));
     
-    /* checking for args to copy them possibly over, otherwise they stay nullified */
-    if(args)
-    {
-        /* copy */
-        memcpy(buffer.req.args, args, sizeof(buffer.req.args));
-    }
-    
-    /* checking for mach ports, if present creating ool descriptor for mach ports */
-    buffer.req.oolp.type = MACH_MSG_OOL_PORTS_DESCRIPTOR;
-    
+    /*
+     * this is used for mach and file descriptor
+     * transmission, for simplicity, you cant
+     * extract ports efficiently from the guest
+     * using its task port, i attempted that
+     * when i wrote copy_in and copy_out and
+     * found that the soloutions are slow and
+     * not feasible. on file descriptors there was
+     * no other sulotion than directly using a ports
+     * descriptor as mach is not BSD and file descriptors
+     * is a bsd and not a mach concept but there is
+     * a API called fileport that can be used to
+     * on iOS convert a file descriptor into a
+     * mach port which is like dup2 on a file
+     * descriptor just that you create a mach port
+     * which can restore the exact same file
+     * descriptor later with the oppositing
+     * fileport api.
+     */
     if(in_ports &&
        in_ports_cnt > 0)
     {
         buffer.req.body.msgh_descriptor_count = 1;
+        buffer.req.oolp.type = MACH_MSG_OOL_PORTS_DESCRIPTOR;
         buffer.req.header.msgh_bits |= MACH_MSGH_BITS_COMPLEX;
         buffer.req.oolp.disposition = in_type;
         buffer.req.oolp.address = in_ports;
@@ -178,28 +202,33 @@ int64_t syscall_invoke(syscall_client_t *client,
      */
     kern_return_t kr = mach_msg(&buffer.req.header, MACH_SEND_MSG | MACH_RCV_MSG, sizeof(syscall_request_t), sizeof(buffer), reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
     
-    /* checking for succession */
     if(kr != KERN_SUCCESS)
     {
+        errno = EBADMSG;
         return -1;
     }
     
-    /* checking for output ports */
+    /*
+     * the kernel can send ports back in a reply
+     * which can contain file ports and mach ports
+     * like task ports if the guest called SYS_gettask
+     */
     if(buffer.reply.oolp.address != VM_MIN_ADDRESS)
     {
-        /* copying ports */
+        /* TODO: more validation prolly needed */
         for(uint32_t c = 0; c < buffer.reply.oolp.count; c++)
         {
             (*out_ports)[c] = ((mach_port_t*)(buffer.reply.oolp.address))[c];
         }
         
-        /* deallocating that nasty mess */
         vm_deallocate(mach_task_self(), (mach_vm_address_t)buffer.reply.oolp.address, buffer.reply.oolp.count * sizeof(mach_port_t));
     }
     
-    /* just set errno */
+    /*
+     * the host usually provides a errno on failure
+     * so we set it as usually and return with its
+     * result.
+     */
     errno = buffer.reply.err;
-    
-    /* done ;3 */
     return buffer.reply.result;
 }
