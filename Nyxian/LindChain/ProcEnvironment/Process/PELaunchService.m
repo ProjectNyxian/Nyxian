6731ed79702f75f170fb19e77461199f7e139fd7
Frida <157828245+cr4zyengineer@users.noreply.github.com>
2026-04-08 21:51:00 +0200

    refactor: removed separate timeout in PELaunchService
    trust in PESpawnTimeout

--- Nyxian/LindChain/ProcEnvironment/Process/PELaunchService.m
+++ Nyxian/LindChain/ProcEnvironment/Process/PELaunchService.m
@@ -79,9 +79,7 @@
     {
         __weak typeof(self) weakSelf = self;
         [_process setExitingCallback:^{
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [weakSelf ignition];
-            });
+            [weakSelf ignition];
         }];
     }
     
