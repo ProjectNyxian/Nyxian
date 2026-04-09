e3f679f0c0e7cf0a9b46804ffcd4994fa8c8b282
Frida <157828245+cr4zyengineer@users.noreply.github.com>
2026-04-09 03:04:17 +0200

    chore: added Simon Støvring to the credits for Runestone
    I totally forgot to do that before.

--- Nyxian/UI/Settings/CreditsViewController.swift
+++ Nyxian/UI/Settings/CreditsViewController.swift
@@ -29,6 +29,7 @@ class CreditsViewController: UIThemedTableViewController {
     private var credits: [Credit] = [
         Credit(name: "Nyxia", role: "Maintainer", githubURL: "https://github.com/cr4zyengineer"),
         Credit(name: "LucaVmu", role: "Manager", githubURL: "https://github.com/LucaVmu"),
+        Credit(name: "Simon Støvring", role: "Runestone", githubURL: "https://github.com/simonbs"),
         Credit(name: "Duy Tran", role: "LiveContainer", githubURL: "https://github.com/khanhduytran0"),
         Credit(name: "Huge_Black", role: "LiveContainer", githubURL: "https://github.com/hugeBlack"),
         Credit(name: "Vinogradov Daniil", role: "Massive help on LLVM-On-iOS", githubURL: "https://github.com/XITRIX"),
@@ -42,6 +43,7 @@ class CreditsViewController: UIThemedTableViewController {
     private var credits: [Credit] = [
         Credit(name: "Nyxia", role: "Maintainer", githubURL: "https://github.com/cr4zyengineer"),
         Credit(name: "LucaVmu", role: "Manager", githubURL: "https://github.com/LucaVmu"),
+        Credit(name: "Simon Støvring", role: "Runestone", githubURL: "https://github.com/simonbs"),
         Credit(name: "Duy Tran", role: "FrontBoardAppLauncher", githubURL: "https://github.com/khanhduytran0"),
         Credit(name: "Vinogradov Daniil", role: "Massive help on LLVM-On-iOS", githubURL: "https://github.com/XITRIX"),
         Credit(name: "light-tech", role: "LLVM-On-iOS", githubURL: "https://github.com/light-tech"),
